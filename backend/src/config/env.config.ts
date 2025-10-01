46447940cd937ecc7ff709bcf68b086ff32a52f7
Arjun <arjunbrt1303@gmail.com>
2025-10-01 23:31:11 +0530

    Initial commit with current progress

---
+++ backend/src/config/env.config.ts
@@ -0,0 +1,17 @@
+// src/config/env.config.ts
+import { getEnv } from "../utils/get-env";
+
+const envConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  PORT: getEnv("PORT", "8000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+  MONGO_URI: getEnv("MONGO_URI"),                       // required, no default
+  JWT_SECRET: getEnv("JWT_SECRET", "secret_jwt"),
+  JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "15m") as string,
+  JWT_REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET", "secret_jwt_refresh"),
+  JWT_REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN", "7d") as string,
+  GEMINI_API_KEY: getEnv("GEMINI_API_KEY"),             // optional, no default
+  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
+}
+);
+export const Env = envConfig();
