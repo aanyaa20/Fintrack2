4d213aea12ce1890370b638e706dfcb61edad0cb
Arjun <arjunbrt1303@gmail.com>
2025-11-19 03:40:33 +0530

    3.3

--- client/src/components/transaction/add-transaction-drawer.tsx
+++ client/src/components/transaction/add-transaction-drawer.tsx
@@ -1,3 +1,60 @@
+// import { Button } from "@/components/ui/button";
+// import {
+//   Drawer,
+//   DrawerClose,
+//   DrawerContent,
+//   DrawerDescription,
+//   DrawerHeader,
+//   DrawerTitle,
+//   DrawerTrigger,
+// } from "@/components/ui/drawer";
+// import { PlusIcon, XIcon } from "lucide-react";
+// import { useState } from "react";
+// import TransactionForm from "./transaction-form";
+
+// const AddTransactionDrawer = () => {
+//   const [open, setOpen] = useState(false);
+
+//   const onCloseDrawer = () => {
+//     setOpen(false);
+//   };
+
+
+//   return (
+//     <Drawer  direction="right" open={open} onOpenChange={setOpen}>
+//       <DrawerTrigger asChild>
+//         <Button className="!cursor-pointer !text-white">
+//           <PlusIcon className="h-4 w-4" />
+//           Add Transaction
+//         </Button>
+//       </DrawerTrigger>
+//       <DrawerContent className="max-w-md overflow-hidden overflow-y-auto">
+//         <DrawerHeader className="relative">
+//          <div>
+//           <DrawerTitle className="text-xl font-semibold">
+//               Add Transaction
+//             </DrawerTitle>
+//             <DrawerDescription>
+//               Add a new transaction to track your finances
+//             </DrawerDescription>
+//          </div>
+//           <DrawerClose className="absolute top-4 right-4">
+//             <XIcon className="h-5 w-5 !cursor-pointer" />
+//           </DrawerClose>
+//         </DrawerHeader>
+//         <TransactionForm 
+//         onCloseDrawer={onCloseDrawer}
+//         />
+//       </DrawerContent>
+//     </Drawer>
+//   );
+// };
+
+// export default AddTransactionDrawer;
+
+
+
+
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -8,9 +65,9 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import TransactionForm from "./transaction-form";
 import { PlusIcon, XIcon } from "lucide-react";
 import { useState } from "react";
-import TransactionForm from "./transaction-form";
 
 const AddTransactionDrawer = () => {
   const [open, setOpen] = useState(false);
@@ -19,9 +76,8 @@ const AddTransactionDrawer = () => {
     setOpen(false);
   };
 
-
   return (
-    <Drawer  direction="right" open={open} onOpenChange={setOpen}>
+    <Drawer direction="right" open={open} onOpenChange={setOpen} modal={false}>
       <DrawerTrigger asChild>
         <Button className="!cursor-pointer !text-white">
           <PlusIcon className="h-4 w-4" />
@@ -30,21 +86,19 @@ const AddTransactionDrawer = () => {
       </DrawerTrigger>
       <DrawerContent className="max-w-md overflow-hidden overflow-y-auto">
         <DrawerHeader className="relative">
-         <div>
-          <DrawerTitle className="text-xl font-semibold">
+          <div>
+            <DrawerTitle className="text-xl font-semibold">
               Add Transaction
             </DrawerTitle>
             <DrawerDescription>
               Add a new transaction to track your finances
             </DrawerDescription>
-         </div>
+          </div>
           <DrawerClose className="absolute top-4 right-4">
             <XIcon className="h-5 w-5 !cursor-pointer" />
           </DrawerClose>
         </DrawerHeader>
-        <TransactionForm 
-        onCloseDrawer={onCloseDrawer}
-        />
+        <TransactionForm onCloseDrawer={onCloseDrawer} />
       </DrawerContent>
     </Drawer>
   );
